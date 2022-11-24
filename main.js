2498cd73f7f34c59790568ef4fcfc466fd413127
dakanadaka <113799440+EnergeticBubble@users.noreply.github.com>
2022-11-24 16:12:44 +0100

    env filke to work on linux

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
+const dotenv = require('dotenv');
+dotenv.config({ path: './.ENV' });
 
 const app = express();
 const port = process.env.PORT || 5000;
