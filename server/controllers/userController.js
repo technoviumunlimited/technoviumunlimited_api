b4fb2feabdaea52cb3feb6c46c0ff9ed10856e8f
dakanadaka <113799440+EnergeticBubble@users.noreply.github.com>
2022-11-25 14:32:24 +0100

    signup fix on many validation points
    Luuk needs to see this

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 require("../models/db");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const validator = require('validator');
 
 /**
  * /api/user/{id}
@@ -25,7 +26,7 @@ exports.getUser = async (req, res) => {
 exports.getAllUsers = async (req, res) => {
 
    try {
-      const users = await User.find({});
+      const users = await User.find();
       res.json(users);
 
    } catch (err) {
@@ -36,34 +37,98 @@ exports.getAllUsers = async (req, res) => {
 /**
  * /api/user/signup/{username}
  * POST create new user
+ * https://github.com/gitdagray/mongo_async_crud/blob/main/controllers/registerController.js
  */
 
 exports.createUser = async (req, res) => {
+   var errors = [];
+   const { username, email, password, date_of_birth, gender } = req.body;
 
-   const hashedPass = await bcrypt.hash(req.body.password, 10);
+   if(!username) {
+      var username_error = {'name' : 'username', 'status': '400', 'message' : 'Username is required.'};
+      errors.push(username_error);
+   }
+   
+   if(!validator.isAlphanumeric(username)){
+      var username_alphanumeric_error = {'name' : 'username_alphanumeric', 'status': '400', 'message' : 'Username needs to be combination of numbers and letters.'};
+      errors.push(username_alphanumeric_error);
+   }
 
-   User.findOne({
+   if(!email) {
+      var email_error = {'name' : 'email', 'status': '400', 'message' : 'Email is required.'};
+      errors.push(email_error);
+   }
+
+   if(!validator.isEmail(email)) {
+      var email_error = {'name' : 'email', 'status': '400', 'message' : 'The Email Address is Not a Well Formed E-mail address'};
+      errors.push(email_error);
+   }
+
+   if(!password) {
+      var password_error = {'name' : 'password', 'status': '400', 'message' : 'Password is required.'};
+      errors.push(password_error);
+   }
+
+   if(!date_of_birth) {
+      var date_of_birth_error = {'name' : 'date_of_birth', 'status': '400', 'message' : 'Date of birth is required.'};
+      errors.push(date_of_birth_error);
+   }
+
+   if(!validator.isDate(date_of_birth)) {
+      var date_of_birth_format_error = {'name' : 'date_of_birth_format', 'status': '400', 'message' : 'Date of birth is not formated ex. YYYY/MM/DD.'};
+      errors.push(date_of_birth_format_error);
+   }
+   
+
+   if(!gender) {
+      var gender_error = {'name' : 'gender', 'status': '400', 'message' : 'Gender is required.'};
+      errors.push(gender_error);
+   }
+
+   
+   const email_duplicate = await User.findOne({
       email: req.body.email
-   })
-   .exec((err,user) => {
-      if(err){ return res.status(500).send({ message: err }); }
-      if(user){ return res.status(403).send({ message: "Email is already in use." }); }
-   });
+   }).exec();
 
-   const newUser = new User({
-      username: req.body.username,
-      email: req.body.email.toLowerCase(),
-      password: hashedPass,
-      date_of_birth: req.body.date_of_birth,
-      gender: req.body.gender,
-      status: req.body.status
-   });
+   if (email_duplicate) {
+      var username_duplicate_error = {'name' : 'email_duplicate', 'status': '403', 'message' : 'Email is already in use.'};
+      errors.push(username_duplicate_error);
+   }
+   
+   const username_duplicate = await User.findOne({
+      username: req.body.username
+   }).exec();
+
+   if (username_duplicate) {
+      var username_duplicate_error = {'name' : 'username_duplicate', 'status': '403', 'message' : 'Username is already in use.'};
+      errors.push(username_duplicate_error);
+   }
+
+   if(errors.length > 0){
+      return res.status(400).json([{ "message": "errors"}, errors]);
+   }
 
    try {
+      const hashedPass = await bcrypt.hash(req.body.password, 10);
+      
+      const newUser = new User({
+         username:      req.body.username.toLowerCase(),
+         email:         req.body.email.toLowerCase(),
+         password:      hashedPass,
+         date_of_birth: req.body.date_of_birth,
+         gender:        req.body.gender,
+         status:        "waiting_for_email_verification" // waiting_for_email_verification, live, deleted
+      });
+
       await newUser.save();
-      res.json(newUser);
+      var return_message = {
+         'message': 'success',
+         'user_id': newUser._id
+      }
+      return res.json(return_message);
+
    } catch (err) {
       console.log(err.message);
-      res.status(400).json({ message: err });
+      return res.status(400).json({ message: err });
    }
 };
